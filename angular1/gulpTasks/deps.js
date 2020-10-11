4ff107f2e79e44ad630aeccf11dc22e3c6dea36a
Wellyngton Carvalho <welitoncarvalho.dev@gmail.com>
2020-10-11 19:19:27 -0300

    criando serviços do gulp

---
+++ angular1/gulpTasks/deps.js
@@ -0,0 +1,16 @@
+const GulpClient = require('gulp')
+const gulp = require('gulp')
+
+gulp.task('deps', ['deps.js', 'deps.css', 'deps.fonts'])
+
+gulp.task('deps.js', () => {
+
+})
+
+gulp.task('deps.css', () => {
+
+})
+
+gulp.task('deps.fonts', () => {
+    
+})
