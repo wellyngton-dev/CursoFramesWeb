aee2d5795e006b177b4ae283a082cb496c6ddec5
Wellyngton Carvalho <welitoncarvalho.dev@gmail.com>
2020-10-21 22:08:23 -0300

    Calculos ciclo de pagamentos

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -10,15 +10,16 @@
         const vm = this
         const url = 'http://localhost:3003/api/billingCycles'
 
-        vm.refresh = function () {
+        vm.refresh = () => {
             $http.get(url).then(function (response) {
                 vm.billingCycle = { credits: [{}], debts: [{}] }
                 vm.billingCycles = response.data
+                vm.calculateValues()
                 tabs.show(vm, { tabList: true, tabCreate: true })
             })
         }
 
-        vm.create = function () {
+        vm.create = () => {
             $http.post(url, vm.billingCycle).then(function (response) {
                 vm.refresh()
                 vm.billingCycle = response.data
@@ -28,18 +29,20 @@
             })
         }
 
-        vm.showTabUpdate = function (billingCycle) {
+        vm.showTabUpdate = (billingCycle) => {
             vm.billingCycle = billingCycle
+            vm.calculateValues()
             tabs.show(vm, { tabUpdate: true })
 
         }
 
-        vm.showTabDelete = function (billingCycle) {
+        vm.showTabDelete = (billingCycle) => {
             vm.billingCycle = billingCycle
+            vm.calculateValues()
             tabs.show(vm, { tabDelete: true })
         }
 
-        vm.update = function () {
+        vm.update = () => {
             const updateUrl = `${url}/${vm.billingCycle._id}`
             $http.put(updateUrl, vm.billingCycle).then(function (response) {
                 vm.refresh()
@@ -50,7 +53,7 @@
             })
         }
 
-        vm.delete = function () {
+        vm.delete = () => {
             const deleteUrl = `${url}/${vm.billingCycle._id}`
             $http.delete(deleteUrl, vm.billingCycle).then(function (response) {
                 vm.refresh()
@@ -61,6 +64,52 @@
             })
         }
 
+        vm.addCredit = (index) => {
+            vm.billingCycle.credits.splice(index + 1, 0, {})
+
+        }
+
+        vm.cloneCredit = (index, { name, value }) => {
+            vm.billingCycle.credits.splice(index + 1, 0, { name, value })
+            vm.calculateValues()
+        }
+
+        vm.deleteCredit = (index) => {
+            if (vm.billingCycle.credits.length > 1) {
+                vm.billingCycle.credits.splice(index, 1)
+                vm.calculateValues()
+            }
+        }
+
+        vm.addDebt = (index) => {
+            vm.billingCycle.debts.splice(index + 1, 0, {})
+        }
+
+        vm.cloneDebt = (index, { name, value, status }) => {
+            vm.billingCycle.debts.splice(index + 1, 0, { name, value, status })
+            vm.calculateValues()
+        }
+
+        vm.deleteDebt = (index) => {
+            if (vm.billingCycle.debts.length > 1) {
+                vm.billingCycle.debts.splice(index, 1)
+                vm.calculateValues()
+            }
+        }
+
+        vm.calculateValues = () => {
+            vm.credit = 0
+            vm.debt = 0
+            if (vm.billingCycle) {
+                vm.billingCycle.credits.forEach(({ value }) => {
+                    vm.credit += !value || isNaN(value) ? 0 : parseFloat(value)
+                })
+                vm.billingCycle.debts.forEach(({ value }) => {
+                    vm.debt += !value || isNaN(value) ? 0 : parseFloat(value)
+                })
+            }
+            vm.total = vm.credit - vm.debt
+        }
         vm.refresh()
     }
 })()
