6473476418962207971738a0c96dcbd306ec91c0
Wellyngton Carvalho <welitoncarvalho.dev@gmail.com>
2020-11-08 12:31:12 -0300

    create users area in frontend

---
+++ angular1/app/auth/authController.js
@@ -0,0 +1,19 @@
+(function () {
+
+    angular.module('primeiraApp').controller('AuthCtrl', [
+        '$location',
+        'msgs',
+        AuthController
+    ])
+
+    function AuthController($location, msgs) {
+        const vm = this
+
+        vm.getUser = () => ({ name: 'Usuário MOCK', email: 'mock@cod3r.com.br' })
+
+        vm.logout = () => {
+            console.log('Logout...')
+        }
+    }
+
+})()
