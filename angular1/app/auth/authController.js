abd635aa523a5fff4fc027e2bd2c4142384b502d
Wellyngton Carvalho <welitoncarvalho.dev@gmail.com>
2020-11-18 22:06:14 -0300

    autenticação implementada

--- angular1/app/auth/authController.js
+++ angular1/app/auth/authController.js
@@ -3,17 +3,26 @@
     angular.module('primeiraApp').controller('AuthCtrl', [
         '$location',
         'msgs',
+        'auth',
         AuthController
     ])
 
-    function AuthController($location, msgs) {
+    function AuthController($location, msgs, auth) {
         const vm = this
 
-        vm.getUser = () => ({ name: 'Usuário MOCK', email: 'mock@cod3r.com.br' })
+        vm.loginMode = true
+        vm.changeMode = () => vm.loginMode = !vm.loginMode
+        vm.login = () => {
+            auth.login(vm.user, err => err ? msgs.addError(err) : $location.path('/'))
+        }
+        vm.signup = () => {
+            auth.signup(vm.user, err => err ? msgs.addError(err) : $location.path('/'))
+        }
+
+        vm.getUser = () => auth.getUser()
 
         vm.logout = () => {
-            console.log('Logout...')
+            auth.logout(() => $location.path('/'))
         }
     }
-
 })()
--- angular1/app/config/routes.js
+++ angular1/app/config/routes.js
@@ -1,8 +1,9 @@
-(function () {  
+(function () {
     angular.module('primeiraApp').config([
         '$stateProvider',
         '$urlRouterProvider',
-        function ($stateProvider, $urlRouterProvider) {
+        '$httpProvider',
+        function ($stateProvider, $urlRouterProvider, $httpProvider) {
             $stateProvider.state('dashboard', {
                 url: "/dashboard",
                 templateUrl: "dashboard/dashboard.html"
@@ -11,7 +12,37 @@
                 templateUrl: "billingCycle/tabs.html"
             })
 
-            $urlRouterProvider.otherwise('/dashboard')
-        }
-    ])
+            $httpProvider.interceptors.push('handleResponseError')
+        }])
+        .run([
+            '$rootScope',
+            '$http',
+            '$location',
+            '$window',
+            'auth',
+            function ($rootScope, $http, $location, $window, auth) {
+                validateUser()
+                $rootScope.$on('$locationChangeStart', () => validateUser())
+
+                function validateUser() {
+                    const user = auth.getUser()
+                    const authPage = '/auth.html'
+                    const isAuthPage = $window.location.href.includes(authPage)
+
+                    if (!user && !isAuthPage) {
+                        $window.location.href = authPage
+                    } else if (user && !user.isValid) {
+                        auth.validateToken(user.token, (err, valid) => {
+                            if (!valid) {
+                                $window.location.href = authPage
+                            } else {
+                                user.isValid = true
+                                $http.defaults.headers.common.Authorization = user.token
+                                isAuthPage ? $window.location.href = '/' : $location.path('/dashboard')
+                            }
+                        })
+                    }
+                }
+            }
+        ])
 })()
